e13b7fa77e22a5db4fa1bde71040d2bb8c1ed715
Haaxor1689 <betkomaros@gmail.com>
2022-11-30 00:28:42 +0100

    Improved prisma action

--- scripts/db/prisma.mjs
+++ scripts/db/prisma.mjs
@@ -1,8 +1,10 @@
 // @ts-check
+import { exec } from 'child_process';
+
 import fs from 'fs-extra';
 
 import Config from '../config.mjs';
-import { exec, ScriptDirname } from '../utils.mjs';
+import { ScriptDirname } from '../utils.mjs';
 
 import initDb from './init.mjs';
 
@@ -10,8 +12,14 @@ const prismaClient = async () => {
 	if (!fs.existsSync(`${Config().PatchPath}/DBFilesClient`))
 		throw 'No DBFilesClient folder...';
 
-	initDb();
-	await exec(`npx prisma studio --schema=${ScriptDirname}/schema.prisma`);
+	await initDb();
+
+	console.log('Starting Prisma Studio...');
+	const prisma = exec(
+		`npx prisma studio --schema=${ScriptDirname}/schema.prisma`
+	);
+
+	prisma.stdout?.on('data', d => d.trim() && console.log(d.trim()));
 };
 
 export default prismaClient;
